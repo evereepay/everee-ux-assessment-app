b5df3c0a467d46505729cf845782dc57cf3eb129
lukeschunk <lukeschunk@gmail.com>
2022-05-31 16:50:07 -0600

    sets up assessmet repo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import { data } from "./data";
+
+function App() {
+  return <div className="App">hello everee ux assessment</div>;
+}
+
+export default App;
---
+++ src/data.ts
@@ -0,0 +1,43 @@
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export const data: IUser[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@test.com",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    email: "janedoe@test.com",
+  },
+  {
+    id: 3,
+    name: "Luke Schunk",
+    email: "lukeschunk@test.com",
+  },
+  {
+    id: 4,
+    name: "Ian Schoen",
+    email: "ianschoen@test.com",
+  },
+  {
+    id: 5,
+    name: "Chris Allan",
+    email: "chrisallan@test.com",
+  },
+  {
+    id: 6,
+    name: "Ron Ross",
+    email: "ronross@test.com",
+  },
+  {
+    id: 7,
+    name: "Cutler Simpson",
+    email: "cutlersimpson@test.com",
+  },
+];
